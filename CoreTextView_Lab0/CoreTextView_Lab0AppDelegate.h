fdf82cc065cdec5471dd943cc196861e543a6b22
Deheng Xu <xudeheng_nx@yahoo.com.cn>
2011-11-11 15:33:12 +0800

    Initial Commit

---
+++ CoreTextView_Lab0/CoreTextView_Lab0AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CoreTextView_Lab0AppDelegate.h
+//  CoreTextView_Lab0
+//
+//  Created by Deheng Xu on 11-11-11.
+//  Copyright 2011年 北京易天新动网络科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoreTextView_Lab0AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CoreTextView_Lab0/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CoreTextView_Lab0
+//
+//  Created by Deheng Xu on 11-11-11.
+//  Copyright 2011年 北京易天新动网络科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CoreTextView_Lab0AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CoreTextView_Lab0AppDelegate class]));
+    }
+}
